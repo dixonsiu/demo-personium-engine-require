b820a661277cdf1048b00c393b823e5f76ba4c65
Dixon Siu <siu.dixon@jp.fujitsu.com>
2018-04-12 14:21:01 +0900

    CHORE: Adjust try/catch scope

--- test.js
+++ test.js
@@ -1,29 +1,29 @@
 function(request){
-    var bodyAsString = request["input"].readAll();
-    if (bodyAsString === "") {
-        var tempBody = {
-            code: "PR400-OD-0006",
-            message: {
-                lang: "en",
-                value: "Request body is empty."
-            }
-        };
-        return loadTest.personium.createResponse(400, tempBody);
-    }
-    var params = JSON.parse(bodyAsString);
-    
-    // Compare and see if unsupportd keys exist or not
     try {
+        var bodyAsString = request["input"].readAll();
+        if (bodyAsString === "") {
+            var tempBody = {
+                code: "PR400-OD-0006",
+                message: {
+                    lang: "en",
+                    value: "Request body is empty."
+                }
+            };
+            return loadTest.personium.createResponse(400, tempBody);
+        }
+        var params = JSON.parse(bodyAsString);
+    
+        // Compare and see if unsupportd keys exist or not
         var allowedKeys = getAllowedKeys();
         loadTest.personium.setAllowedKeys(allowedKeys);
         loadTest.personium.validateKeys(params);
+
+        params.status="success";
+
+        return loadTest.personium.createResponse(201, params);
     } catch(e) {
         return loadTest.personium.createErrorResponse(e);
     }
-    
-    params.status="success";
-
-    return loadTest.personium.createResponse(201, params);
 }
 
 var getAllowedKeys = function() {
